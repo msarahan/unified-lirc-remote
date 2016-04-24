cd12fbe58f92d43bfe9c439bac04af84093b94b8
Michael Sarahan <msarahan@gmail.com>
2016-04-23 20:41:31 -0500

    add start of remote fleshing out

--- remote.lua
+++ remote.lua
@@ -6,21 +6,28 @@
 -- OS Library
 -- http://www.unifiedremote.com/api/libs/os
 
-
---@help Command 1
-actions.command1 = function ()
-	os.start("calc");
+--@help Geezer TV Power
+actions.geezer_tv_power = function ()
+	os.start("irsend SEND_ONCE vizio KEY_POWER");
 end
 
-
---@help Command 2
-actions.command2 = function ()
-	os.start("C:\\foobar.exe");
+--@help TV Power
+actions.tv_power = function ()
+	os.start("irsend SEND_ONCE sceptre KEY_POWER");
 end
 
+--@help Blu-Ray Power
+actions.bluray_power = function ()
+	os.start("irsend SEND_ONCE samsung KEY_POWER");
+end
 
---@help Command 3
-actions.command3 = function ()
-	os.start("ipconfig", "/all");
+--@help Volume Up
+actions.volume_up = function ()
+	os.start("irsend SEND_ONCE onkyo KEY_VOLUMEUP");
+end
+
+--@help Volume Down
+actions.volume_down = function ()
+	os.start("irsend SEND_ONCE onkyo KEY_VOLUMEDOWN");
 end
 
