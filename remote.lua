a95eb5c492374ff500dd90727869b2315bda44c5
Michael Sarahan <msarahan@gmail.com>
2016-04-23 21:22:59 -0500

    try script.shell for running commands

--- remote.lua
+++ remote.lua
@@ -8,46 +8,46 @@
 
 --@help Geezer TV Power
 actions.geezer_tv_power = function ()
-	os.start("irsend SEND_ONCE vizio KEY_POWER");
+	script.shell("irsend SEND_ONCE vizio KEY_POWER");
 end
 
 --@help TV Power
 actions.tv_power = function ()
-	os.start("irsend SEND_ONCE sceptre KEY_POWER");
+	script.shell("irsend SEND_ONCE sceptre KEY_POWER");
 end
 
 --@help Blu-Ray Power
 actions.bluray_power = function ()
-	os.start("irsend SEND_ONCE samsung KEY_POWER");
+	script.shell("irsend SEND_ONCE samsung KEY_POWER");
 end
 
 --@help Volume Up
 actions.volume_up = function ()
-	os.start("irsend SEND_ONCE onkyo KEY_VOLUMEUP");
+	script.shell("irsend SEND_ONCE onkyo KEY_VOLUMEUP");
 end
 
 --@help Volume Up - start
 actions.volume_up_start = function ()
-	os.start("irsend SEND_START onkyo KEY_VOLUMEUP");
+	script.shell("irsend SEND_START onkyo KEY_VOLUMEUP");
 end
 
 --@help Volume Up - stop
 actions.volume_up_stop = function ()
-	os.start("irsend SEND_STOP onkyo KEY_VOLUMEUP");
+	script.shell("irsend SEND_STOP onkyo KEY_VOLUMEUP");
 end
 
 --@help Volume Down
 actions.volume_down = function ()
-	os.start("irsend SEND_ONCE onkyo KEY_VOLUMEDOWN");
+	script.shell("irsend SEND_ONCE onkyo KEY_VOLUMEDOWN");
 end
 
 --@help Volume Down - start
 actions.volume_down_start = function ()
-	os.start("irsend SEND_START onkyo KEY_VOLUMEDOWN");
+	script.shell("irsend SEND_START onkyo KEY_VOLUMEDOWN");
 end
 
 --@help Volume Down - stop
 actions.volume_down_stop = function ()
-	os.start("irsend SEND_STOP onkyo KEY_VOLUMEDOWN");
+	script.shell("irsend SEND_STOP onkyo KEY_VOLUMEDOWN");
 end
 
